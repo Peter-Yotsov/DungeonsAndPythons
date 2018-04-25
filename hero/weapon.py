28fef6e776ccb3b09676e7699ecc660807c318c4
Peter <peteryotsov@abv.bg>
2018-04-25 19:43:06 +0300

    rmv redundancies

--- hero/weapon.py
+++
@@ -1,7 +0,0 @@
-class Weapon:
-    def __init__(self, *, name, damage):
-        self.name = name
-        self.damage = damage
-
-    def __repr__(self):
-        return '{} deals {} damage.'.format(self.name, self.damage)
