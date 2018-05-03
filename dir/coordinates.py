edf10376a1b41dda0c1bffc6455b646b8fccce1d
Ognyanka Boneva <ogi.boneva@gmail.com>
2018-05-04 01:13:57 +0300

    __eq__ added

--- dir/coordinates.py
+++ dir/coordinates.py
@@ -2,3 +2,6 @@ class Coordinates:
     def __init__(self, row, column):
         self.row = row
         self.column = column
+
+    def __eq__(self, other):
+        return self.row == other.row and self.column == other.column
